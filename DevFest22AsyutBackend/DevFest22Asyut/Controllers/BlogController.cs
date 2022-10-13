fb0fce140bc8a6c7337dfbd070635904333e7b65
a-mamdouh <ahmadmamdouh3@gmail.com>
2022-10-13 03:09:50 +0200

    Backend change: Add first DB migration.

--- DevFest22AsyutBackend/DevFest22Asyut/Controllers/BlogController.cs
+++ DevFest22AsyutBackend/DevFest22Asyut/Controllers/BlogController.cs
@@ -24,6 +24,7 @@ namespace DevFest22Asyut.Controllers
             )
         {
             _blogService = blogService;
+            _environment = environment;
         }
 
         public IActionResult Index()
